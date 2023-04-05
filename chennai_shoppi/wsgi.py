20b4fc53bdf4a37523855e0f8d3fae3078d02afe
M.Z.MOHAMMED MOHIDEEN <technicalaasaan@gmail.com>
2023-04-05 16:46:32 +0530

    static loader

--- chennai_shoppi/wsgi.py
+++ chennai_shoppi/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chennai_shoppi.settings')
 
-application = get_wsgi_application()
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
